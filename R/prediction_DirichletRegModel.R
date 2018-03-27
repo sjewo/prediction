b18c40a8974df6da09ef000baeeef27c4ad91a89
Sebastian Jeworutzki <sebastian.jeworutzki@ruhr-uni-bochum.de>
2018-03-27 18:08:00 +0200

    Add support for class DirichletRegModel

---
+++ R/prediction_DirichletRegModel.R
@@ -0,0 +1,53 @@
+#' @rdname prediction
+#' @export
+prediction.DirichletRegModel <- 
+function(model, 
+         data = find_data(model, parent.frame()), 
+         at = NULL, 
+         type = NULL, 
+         calculate_se = FALSE,
+         category,
+         ...) {
+     
+    # extract predicted values
+    data <- data
+    if (missing(data) || is.null(data)) {
+        warning(sprintf("'data' is ignored for models of class '%s'", class(model)))
+    }
+    # setup data
+    if (is.null(at)) {
+        out <- data
+    } else {
+        out <- build_datalist(data, at = at, as.data.frame = TRUE)
+        at_specification <- attr(out, "at_specification")
+    }
+    # calculate predictions
+    tmp <- make_data_frame(predict(model, newdata = out, ...))
+    names(tmp) <- paste0("Pr(", seq_len(ncol(tmp)), ")")
+    # cbind back together
+    pred <- make_data_frame(out, tmp)
+    rm(tmp)
+    
+    # handle category argument
+    if (missing(category)) {
+        w <- grep("^Pr\\(", names(pred))[1L]
+        category <- names(pred)[w]
+        pred[["fitted"]] <- pred[[w]]
+    } else {
+        w <- which(names(pred) == paste0("Pr(", category, ")"))
+        if (!length(w)) {
+            stop(sprintf("category %s not found", category))
+        }
+        pred[["fitted"]] <- pred[[ w[1L] ]]
+    }
+    pred[["se.fitted"]] <- NA_real_
+    
+    # obs-x-(ncol(data)+2+nlevels(outcome)) data frame
+    structure(pred,
+              class = c("prediction", "data.frame"), 
+              row.names = seq_len(nrow(pred)),
+              at = if (is.null(at)) at else at_specification,
+              model.class = class(model),
+              type = NA_character_,
+              category = category)
+}
